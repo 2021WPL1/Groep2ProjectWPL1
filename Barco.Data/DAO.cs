c104629d327d1c9fdc2404e31c490db8baf4be5d
TinaBianca <bianca.capatina@student.vives.be>
2021-03-04 16:55:23 +0100

    new class- Dao Context (work in progress)

---
+++ Barco.Data/DAO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Barco.Data
+{
+    class DAO
+    {
+        private static readonly DAO instance = new DAO();
+
+        public static DAO Instance()
+        {
+            return instance;
+        }
+
+        // Private constructor!
+        private DAO()
+        {
+            this.context= new Barco2021Context();
+        }
+
+       
+        // DBContext
+        private Barco2021Context context;
+
+
+        public void saveChanges()
+        {
+            context.SaveChanges();
+        }
+
+    }
+}
