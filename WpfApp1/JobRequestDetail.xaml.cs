172c17fc1df9e7dc7399b5b5a522e6a28a575e68
Thibaut Deconinck <r0753869@student.vives.be>
2021-03-08 16:36:13 +0100

    jobrequest detail en parts af, EUT NOG NIET

--- WpfApp1/JobRequestDetail.xaml.cs
+++ WpfApp1/JobRequestDetail.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Barco.Data;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -17,9 +18,52 @@ namespace Barco
     /// </summary>
     public partial class JobRequestDetail : Window
     {
+        private DAO dao;
         public JobRequestDetail()
         {
             InitializeComponent();
+            dao = DAO.Instance();
+            load(1002); //TODO bij oproepen van constructor id mee geven
+        }
+
+        //laad de gegevens in van een jobrequest op basis van het id
+        private void load(int selectedId)
+        {
+            RqRequest req = dao.getRequest(selectedId);
+            RqRequestDetail reqdet = dao.getRequestDetail(selectedId);
+            Eut eut = dao.getEut(reqdet.IdRqDetail);
+            RqOptionel optionel = dao.getOptionel(selectedId);
+
+
+            txtRequisterInitials.Text = req.Requester;
+            txtJobNature.Text = req.JobNature;
+            txtDevision.Text = req.BarcoDivision;
+            txtProjectName.Text = req.EutProjectname;
+            lblRequestDate.Content = req.RequestDate;
+            lblExpectedEndDate.Content = req.ExpectedEnddate;
+            lblJobRequestNumber.Content = req.JrNumber;
+            if (req.Battery == true)
+            {
+                RBBatteriesYes.IsChecked = true;
+            }
+            else
+            {
+                RBBatteriesNo.IsChecked = true;
+            }
+
+            txtLinkToTestPlan.Text = optionel.Link;
+            txtSpecialRemarks.Text = optionel.Remarks;
+
+            //do-while extra aanmaken voor partweight met de nieuwe database
+            string s = req.EutPartnumbers;
+            do
+            {
+                ListBoxPartNumber.Items.Add(s.Substring(0, s.IndexOf(";")));
+                s = s.Substring(s.IndexOf(";") + 1);
+
+            } while (s.Contains(";"));
+            ListBoxPartNumber.Items.Add(s);
+
         }
 
        
