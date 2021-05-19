56e36364146482b2db1ddd274443cf175e4bbebc
Jimmy Vilvörder <r0847354@student.vives.be>
2021-05-19 15:05:35 +0200

    Get Division from registery

--- WpfApp1/Views/JobRequest.xaml.cs
+++ WpfApp1/Views/JobRequest.xaml.cs
@@ -48,7 +48,7 @@ namespace Barco
         {
             //cmbDivision.ItemsSource = dao.GetDivisions();
             cmbDivision.Items.Add(getValues("DIVISION"));
-            cmbDivision.SelectedIndex = 0;
+            
 
         }
         public void getJobNatures()
