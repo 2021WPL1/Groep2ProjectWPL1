b501ce6f02f028255ef3a74c0bbe7a4bf7f63814
Jimmy Vilvörder <r0847354@student.vives.be>
2021-05-26 09:12:33 +0200

    appsetteing path

--- WpfApp1/ModelViews/JobRequestViewModel.cs
+++ WpfApp1/ModelViews/JobRequestViewModel.cs
@@ -400,7 +400,7 @@ namespace Barco
 
                     //detail object
                     //Detail.Testdivisie = "eco";  
-                    detailList = GetRqRequestDetails();
+                   var detailList = GetRqRequestDetails();
                     
                     dao.AddRequest(request, detailList, optional, eutList);
                     MessageBox.Show("Data has been inserted");
