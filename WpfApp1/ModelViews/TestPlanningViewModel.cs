3ff08fc39319145282ffd46d8796b7239aa6773b
TinaBianca <bianca.capatina@student.vives.be>
2021-05-26 17:44:52 +0200

    changes Bianca

---
+++ WpfApp1/ModelViews/TestPlanningViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using Barco.Data;
+using Barco.Views;
+using Prism.Commands;
+
+namespace Barco.ModelViews
+{
+    public class TestPlanningViewModel : ViewModelBase
+    {
+        private DAO dao;
+        private TestPlanning screen;
+        public ICommand SaveCommand { get; set; }
+        public ICommand CancelCommand { get; set; }
+
+        public TestPlanningViewModel(TestPlanning screen)
+        {
+            this.screen = screen;
+            SaveCommand = new DelegateCommand(SaveButton);
+            CancelCommand = new DelegateCommand(CancelButton);
+            dao= DAO.Instance();
+
+        }
+
+
+        public void SaveButton()
+        {
+            MessageBox.Show("Congratulations, you have submitted a new test planning.");
+            OverviewPlannedTests overviewPlannedTests = new OverviewPlannedTests();
+            screen.Close();
+            overviewPlannedTests.ShowDialog();
+        }
+
+        public void CancelButton()
+        {
+           PlanningJR planningJr = new PlanningJR();
+            screen.Close();
+
+           planningJr.ShowDialog();
+        }
+
+    }
+
+
+}
+
